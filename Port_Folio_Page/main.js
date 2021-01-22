a8c1d3651a795d40c8cf1e088f1b4308a7de183e
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-01-22 05:57:32 +0100

    Structure of The Whole Proyect-creationOf -> ProFolio

---
+++ Port_Folio_Page/main.js
@@ -0,0 +1,29 @@
+var projects = {
+    0:{
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      },
+    1:{
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      }
+
+}
+var personalData = {
+    0:{
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+      },
+    1: {
+        eco:"eco"
+    }
+}
+
+var newproject = projects[0]["age"];
+console.log(newproject)
