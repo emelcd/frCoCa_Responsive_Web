03fa50fa8edc0f6217ea28f5707ae278dff2e79e
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-01-23 02:35:24 +0100

    Finished Component Project

--- Port_Folio_Page/main.js
+++ Port_Folio_Page/main.js
@@ -2,6 +2,7 @@
 var projects = {
 
     0:{
+        proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Product Landing",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
         proLang: ["js", "html5", "css3"],
@@ -10,6 +11,7 @@ var projects = {
 
       },
     1:{
+        proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Survey Form ",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
         proLang: ["html5", "css3", "js"],
@@ -17,6 +19,7 @@ var projects = {
         proImg: "https://cdn.pixabay.com/photo/2015/10/12/15/18/store-984393_960_720.jpg"
     },
     2:{
+        proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Tribute Page",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
         proLang: ["html5", "css3"],
@@ -24,6 +27,7 @@ var projects = {
         proImg: "https://cdn.pixabay.com/photo/2015/10/12/15/18/store-984393_960_720.jpg"
     },
     3:{
+        proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "PortFolio Json",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
         proLang: ["html5", "css3"],
@@ -31,6 +35,7 @@ var projects = {
         proImg: "https://cdn.pixabay.com/photo/2015/10/12/15/18/store-984393_960_720.jpg"
     },
     4:{
+        proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Tech Documentation",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
         proLang: ["html5", "css3"],
@@ -43,29 +48,42 @@ var projects = {
 var iconsLang = {
     "html5":'<i class="fab fa-html5"></i>',
     "css3":'<i class="fab fa-css3-alt"></i>',
-    "js": '<i class="fab fa-js"></i>',
-    "py": '<i class="fab fa-python"></i>'
+    "js": '<i class="fab fa-js-square"></i>',
+    "py": '<i class="fab fa-python"></i>',
+    "codepen": '<i class="fab fa-codepen"></i>',
+    "git": '<i class="fab fa-github"></i>'
 }
 
 // RETURN ICONS
-function returnHtmlIcon(key) {
-    return iconsLang[key]
-}
-function returnIcons(arrLang) {
+
+function returnIconsLang(arrLang) {
 
     var mapedIcons = arrLang.map(lang => {
         // console.log(lang)
-        return returnHtmlIcon(lang);
+        return iconsLang[lang];
     })
+    console.log(mapedIcons)
     // console.log(mapedIcons)
-    return mapedIcons;
+    return `${mapedIcons.join(" ")}`;
+
+    
+}
+function returnIconsLinks(arrLinks) {
+
+    var mapLinkC = "<a href="+arrLinks.proLink+"><i class='fab fa-codepen'></i></a>";
+    var mapLinkG = "<a href="+arrLinks.proGit+"><i class='fab fa-github'></i></a>";
+    
+    return `${mapLinkC}
+    ${mapLinkG}`;
+    // console.log(mapedIcons)
+    // return `<a href='${mapLinkC}'>${iconsLang[git]}</a>`;
 
     
 }
 // FUNCIONes QUE DEVUELVE PROYECTOS
 
 
-// returnIcons(projects[0].proLang)
+// returnIconsLang(projects[0].proLang)
 function createProjects(pro) {
     var projectDom = document.getElementById("projects");
     Object.keys(pro).forEach(project => {
@@ -79,13 +97,20 @@ function createProjects(pro) {
         projectDom.innerHTML += `
         <div class="project-item">
         <h4 class="project-item-name">${proName}</h4>
+        <hr class="project-hr">
         <a href="${proLink}"><img class="project-item-img" src="${proImg}"></img></a>
         <h6 class="project-item-des">${proDescription}</h6>
-        <div class="project-thumbnail">
-        ${returnIcons(projects[project].proLang)}
+        <div class="project-footer">
+        <div class="project-thumbnail-right">
+        ${returnIconsLang(projects[project].proLang)}
         </div>
-        <div class="product-footer"></div></div>
+        <div class="project-thumbnail-left">
+        ${returnIconsLinks(projects[project])}
+        </div>
+        </div></div>
         `
+        // ${projects[project].proCodeP}
+        // ${projects[project].proGit}
 
     });
     
