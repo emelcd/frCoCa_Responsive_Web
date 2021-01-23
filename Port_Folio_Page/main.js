f4f66801cafb8d1a88e65eca9fde44d6fe5510b9
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-01-23 04:21:48 +0100

    Adding Progress Bar Skill Component

--- Port_Folio_Page/main.js
+++ Port_Folio_Page/main.js
@@ -1,7 +1,13 @@
-// Variables
+// Porfolio DATA
+var personal = {
+    name: "Mick López",
+    jobrole: "Junior Web Developer",
+    resume: "Frontend (Pure HTML/CSS/JS, React, Sass...) and Backend (PHP, Node, MySQL, "
+    // skills: {}
+}
 var projects = {
 
-    0:{
+    0: {
         proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Product Landing",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
@@ -9,8 +15,8 @@ var projects = {
         proLink: "https://codepen.io/mickiticki/pen/ExgOgRw",
         proImg: "https://cdn.pixabay.com/photo/2015/10/12/15/18/store-984393_960_720.jpg"
 
-      },
-    1:{
+    },
+    1: {
         proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Survey Form ",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
@@ -18,7 +24,7 @@ var projects = {
         proLink: "https://codepen.io/mickiticki/pen/ExgOgRw",
         proImg: "https://cdn.pixabay.com/photo/2015/10/12/15/18/store-984393_960_720.jpg"
     },
-    2:{
+    2: {
         proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Tribute Page",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
@@ -26,7 +32,7 @@ var projects = {
         proLink: "https://codepen.io/mickiticki/pen/ExgOgRw",
         proImg: "https://cdn.pixabay.com/photo/2015/10/12/15/18/store-984393_960_720.jpg"
     },
-    3:{
+    3: {
         proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "PortFolio Json",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
@@ -34,7 +40,7 @@ var projects = {
         proLink: "https://codepen.io/mickiticki/pen/ExgOgRw",
         proImg: "https://cdn.pixabay.com/photo/2015/10/12/15/18/store-984393_960_720.jpg"
     },
-    4:{
+    4: {
         proGit: "http.com/mickiticki/frCoCa_Responsive_Web",
         proName: "Tech Documentation",
         proDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur harum tempore quia reiciendis nesciunt consectetur aspernatur unde error doloribus perspiciatis",
@@ -46,53 +52,63 @@ var projects = {
 };
 
 var iconsLang = {
-    "html5":'<i class="fab fa-html5"></i>',
-    "css3":'<i class="fab fa-css3-alt"></i>',
+    "html5": '<i class="fab fa-html5"></i>',
+    "css3": '<i class="fab fa-css3-alt"></i>',
     "js": '<i class="fab fa-js-square"></i>',
     "py": '<i class="fab fa-python"></i>',
     "codepen": '<i class="fab fa-codepen"></i>',
     "git": '<i class="fab fa-github"></i>'
 }
 
+// UTILITY
 // RETURN ICONS
-
 function returnIconsLang(arrLang) {
 
     var mapedIcons = arrLang.map(lang => {
         // console.log(lang)
         return iconsLang[lang];
     })
-    console.log(mapedIcons)
+    // console.log(mapedIcons)
     // console.log(mapedIcons)
     return `${mapedIcons.join(" ")}`;
 
-    
+
 }
+
 function returnIconsLinks(arrLinks) {
 
-    var mapLinkC = "<a href="+arrLinks.proLink+"><i class='fab fa-codepen'></i></a>";
-    var mapLinkG = "<a href="+arrLinks.proGit+"><i class='fab fa-github'></i></a>";
-    
+    var mapLinkC = "<a href=" + arrLinks.proLink + "><i class='fab fa-codepen'></i></a>";
+    var mapLinkG = "<a href=" + arrLinks.proGit + "><i class='fab fa-github'></i></a>";
+
     return `${mapLinkC}
     ${mapLinkG}`;
     // console.log(mapedIcons)
     // return `<a href='${mapLinkC}'>${iconsLang[git]}</a>`;
 
-    
+
 }
-// FUNCIONes QUE DEVUELVE PROYECTOS
+// RETURN WELCOME
+function returnPersonalData(per) {
 
+    console.log(personalDomT)
+    var personalDomT = document.getElementById("welcome-title");
+    personalDomT.innerHTML = `${per.name}<hr class="welcome-hr">`;
 
-// returnIconsLang(projects[0].proLang)
+    var personalDom = document.getElementById("welcome-wrapper");
+    personalDom.innerHTML += `
+    <h3 class='welcome-subtitle'>${per.jobrole}
+    `;
+}
+// RETURN PRODUCTS)
 function createProjects(pro) {
     var projectDom = document.getElementById("projects");
     Object.keys(pro).forEach(project => {
         // Project Variables
-        let proName = pro[project].proName;     
-        let proDescription = pro[project].proDescription;  
-        let proLang = pro[project].proLang;     
-        let proLink = pro[project].proLink;     
-        let proImg = pro[project].proImg;  
+        let proName = pro[project].proName;
+        let proDescription = pro[project].proDescription;
+        let proLang = pro[project].proLang;
+        let proLink = pro[project].proLink;
+        let proImg = pro[project].proImg;
 
         projectDom.innerHTML += `
         <div class="project-wrapper">
@@ -114,9 +130,9 @@ function createProjects(pro) {
         // ${projects[project].proGit}
 
     });
-    
+
 }
 
-
+returnPersonalData(personal)
 
 createProjects(projects)
